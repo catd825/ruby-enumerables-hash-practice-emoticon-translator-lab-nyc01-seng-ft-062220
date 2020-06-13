56afe213cc6c5ba7ac8fe8ffbe32e31384b50636
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 18:32:09 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,4 +1,7 @@
-# require modules here
+# emoticons.yml
+require "yaml"
+emoticons = YML.load_file('emoticons.yml')
+emoticons
 
 def load_library
   # code goes here - should return hash where each key is the name of an emoticon.  each emoticon name should poin tto a nested hash containing an :english key and a :japanese key and point to the english/japanese versions of the emoticon.
