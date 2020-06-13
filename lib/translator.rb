f2cee35e7b0bede7baa443102107fb12ddf7ce70
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 20:54:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -2,17 +2,22 @@ require 'pry'
 require 'yaml'
 
 def load_library(yml)
-  # code goes here - should return hash where each key is the name of an emoticon.  each emoticon name should point to a nested hash containing an :english key and a :japanese key and point to the english/japanese versions of the emoticon.
 
-emoticons = YAML.load_file(yml) #declare emoticons as variable for YAML
+emoticons = YAML.load_file(yml) #declare emoticons as variable for YAML file path
+  
   new_hash = {} #create new hash of YML data
+  
   emoticons.each do |emotion, emoticon| #iterates over emoticons =, emotion as key and emoticon as value 
+  
     new_hash[emotion] = {   #update empty new hash as 
       :english => emoticon[0], #english key accesses first element of emoticon array
       :japanese => emoticon[1] #japanese key accesses second element of emoticon array
     }
+    
   end 
+  
   binding.pry
+  
   new_hash #must return new hash
 end
 
