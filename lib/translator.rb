c496d8e139762c416bf6a00cb9fc67fe653dbabc
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 20:13:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -18,15 +18,13 @@ end
 
 
 def get_japanese_emoticon (yml, eng_emo)
- 
   library = load_library(yml)
-  emoticon = library.keys.find do |key|
-    library[key][:english] == emoticon
-    
-  end
+    library.each do |meaning, idioms|
+      return idioms[:japanese] if idioms[:english] == eng_emo
+    end
   "Sorry, that emoticon was not found"
 end
-
+binding.pry
 
 def get_english_meaning(yml, jap_emo)
   # code goes here
