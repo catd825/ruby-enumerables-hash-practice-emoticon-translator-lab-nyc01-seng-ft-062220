9573402a73d12cca71cb5a29517b72ea03d72c73
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 18:36:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,14 +5,12 @@ emoticons
 
 def load_library
   # code goes here - should return hash where each key is the name of an emoticon.  each emoticon name should poin tto a nested hash containing an :english key and a :japanese key and point to the english/japanese versions of the emoticon.
+    
+    
 
-    {emoticon => {
-      :english => "",
-      :japanese => ""
-    }
-  }
 
 end
+
 
 def get_japanese_emoticon
   # code goes here
