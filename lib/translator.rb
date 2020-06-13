8dd4d92fe835a36d03edce844a84a2a7f8834893
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 20:11:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -24,8 +24,7 @@ def get_japanese_emoticon (yml, emoticon)
     library[key][:english] == emoticon
     
   end
-  emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
-
+  "Sorry, that emoticon was not found"
 end
 
 
