19debc049ed95fc0bf10b4564833a1d665e70e65
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 20:05:27 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon (yml, emoticon)
   end
   emoticon ? library[emoticon][:japanese] : "Sorry, that emoticon was not found"
 end
-
+binding.pry
 
 def get_english_meaning(yml, emoticon)
   # code goes here
