04c55eda99c5d01b7316d4fcf5844b2685187701
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 20:33:40 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -19,11 +19,11 @@ end
 
 def get_japanese_emoticon (yml, eng_emo) #pass through file path and english emoticon
   library = load_library(yml) #define variable library as passing the YML file through previously created load_library method, which converts the YML file into a nested hash
-    library.each do |definition, emo_hash| #iterate over the nested hash, using the definition and the emoticon hash 
-      return definition[:japanese] if emo_hash[:english] == eng_emo
+    library.each do |definition, emo_hash| #iterate over the nested hash, using the definition and the emoticon hash (hash contains :eng => emoticon, :jap => emoticon)
+      return definition[:japanese] if emo_hash[:english] == eng_emo #if the english emoticon entered exists, return associated japanese emoticon
     binding.pry
     end
-  "Sorry, that emoticon was not found"
+  "Sorry, that emoticon was not found" #error message if english emoticon entered does not exist
 end
 
 
