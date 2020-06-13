2142b3f2780628a8b38deb5422f2218e90bb07a0
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 20:59:23 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,11 +15,9 @@ emoticons = YAML.load_file(yml) #declare emoticons as variable for YAML file pat
     }
     
   end 
-  
-  
   new_hash #must return new hash
 end
-
+binding.pry
 
 
 def get_japanese_emoticon (yml, eng_emoticon) 
