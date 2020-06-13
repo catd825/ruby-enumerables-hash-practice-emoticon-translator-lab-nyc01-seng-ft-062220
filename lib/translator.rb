c666a9cef82657596e09a36ea6b0a19723775eb3
Cathy D'Onofrio <catd825@gmail.com>
2020-06-13 20:41:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,9 +31,11 @@ def get_japanese_emoticon (yml, west_emo) #pass through file path and western em
       
       #if the english emoticon entered exists, return associated japanese emoticon
     
-    binding.pry
+
     
     end
+     binding.pry
+     
   "Sorry, that emoticon was not found" #error message if english emoticon entered does not exist
 end
 
